11dfa1bec5cbd1551e804b7abd9606c705432cdb
22mca047 <22mca047@nirmauni.ac.in>
2025-04-07 13:03:09 +0530

    updated files

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_restx import Api
 from models import Recipe, User
 from exts import db
@@ -7,6 +7,7 @@ from flask_jwt_extended import JWTManager
 from recipes import recipe_ns
 from auth import auth_ns
 from flask_cors import CORS
+import os
 
 def create_app(config):
     app = Flask(__name__)
@@ -18,6 +19,16 @@ def create_app(config):
     api = Api(app, doc='/docs') 
     api.add_namespace(recipe_ns)
     api.add_namespace(auth_ns)
+
+    frontend_folder = os.path.join(os.getcwd(),"..","client")
+    dist_folder = os.path.join(frontend_folder,"build")
+
+    @app.route("/", defaults={"filename":""})
+    @app.route("/<path:filename>")
+    def index(filename):
+        if not filename:
+            filename = "index.html"
+        return send_from_directory(dist_folder, filename)
 
     @app.shell_context_processor
     def make_shell_context():
